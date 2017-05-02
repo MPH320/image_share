d2bdab065d869a225c5881b7eaae924c525c402a
Michelangelo <michelangelo.ph@gmail.com>
2017-05-01 18:48:57 -0700

    First commit

---
+++ client/startup.js
@@ -0,0 +1,23 @@
+//
+//	Meteor.startup(function(){
+//		if(Images.find().count() == 0){
+//			 Images.insert(
+//				 {
+//						img_src:"laptops.jpg",
+//						img_alt:"some laptops on a table" 
+//				 }
+//			 );
+//			 Images.insert(
+//				 {
+//						img_src: "IMG_2509.JPG",
+//						img_alt: "10,000 miles away"
+//					}
+//			 );
+//			 Images.insert(
+//				 {
+//						img_src: "IMG_2879.JPG",
+//						img_alt: "Red"
+//					}
+//			 );
+//		}
+//	});
---
+++ server/main.js
@@ -0,0 +1,24 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+	
+	Images = new Mongo.Collection("images");
+	
+	if (Images.find().count() ==0 ){
+			
+				for (var i = 1; i<9; i++) {
+					Images.insert(
+							 {
+									img_src: "IMG_"+i+".JPG",
+									img_alt: "Image number"+i
+								}
+						 );
+				}
+			
+			
+			
+	}
+
+console.log(Images.find().count());
+
+});
