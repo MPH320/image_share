f0f0dbcd9d4c68f04ec6e96984d6ce184ffec25e
Michelangelo <michelangelo.ph@gmail.com>
2017-05-06 18:45:43 -0700

    Infinite scroll

--- client/main.js
+++ client/main.js
@@ -5,6 +5,28 @@ var big_image = "";
 
 if (Meteor.isClient){
 	
+	Session.set("imageLimit", 8);
+	lastScrollTop = 0;
+	
+	$(window).scroll(function(event){
+
+		if($(window).scrollTop() + $(window).height() > $(document).height() - 100)
+		{
+			var scrollTop = $(this).scrollTop();
+			
+			if (scrollTop > lastScrollTop){
+				
+				Session.set("imageLimit", Session.get("imageLimit") + 4);
+			}
+			
+			lastScrollTop = scrollTop;
+				
+		}
+		
+		
+		
+	});
+	
 	Accounts.ui.config({
 		passwordSignupFields: "USERNAME_AND_EMAIL"
 	});
@@ -18,7 +40,7 @@ if (Meteor.isClient){
 			{
 				 return Images.find({createdBy:Session.get("userFilter")}, {sort:{createdOn: -1, rating:-1}})
 			} else {
-				 	return Images.find({}, {sort:{createdOn: -1, rating:-1}})
+				 	return Images.find({}, {sort:{createdOn: -1, rating:-1}, limit:Session.get("imageLimit")})
 			}
 			
 		},
